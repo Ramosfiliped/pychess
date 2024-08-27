022c279ee8c07282bd13ebf641555a2fad1334fc
Filipe Ramos <filiperamosdesouza@gmail.com>
2024-08-26 23:41:24 -0300

    Design Patterns

--- game.py
+++ game.py
@@ -29,6 +29,62 @@ class Chess:
     self.medium_font = pygame.font.Font('freesansbold.ttf', 40)
     self.big_font = pygame.font.Font('freesansbold.ttf', 50)
 
+  def draw_board(self):
+    for row in range(8):
+      for column in range(8):
+        if row % 2 == 0:
+          pygame.draw.rect(
+            surface=self.screen, 
+            color='light gray', 
+            rect=[600 - (column * 200), row * 100, 100, 100]
+          )
+        else:
+          pygame.draw.rect(
+            surface=self.screen, 
+            color='light gray', 
+            rect=[700 - (column * 200), row * 100, 100, 100]
+          )
+
+        pygame.draw.rect(
+          surface=self.screen, 
+          color='gray', 
+          rect=[0, 800, WIDTH, 100]
+        )
+
+        pygame.draw.rect(
+          surface=self.screen, 
+          color='gold', 
+          rect=[0, 800, WIDTH, 100], 
+          width=5
+        )
+
+        pygame.draw.rect(
+          surface=self.screen, 
+          color='gold', 
+          rect=[800, 0, 200, HEIGHT], 
+          width=5
+        )
+
+        status_text = [
+          'White: Select a Piece to Move!', 'White: Select a Destination!',
+          'Black: Select a Piece to Move!', 'Black: Select a Destination!'
+        ]
+
+        self.screen.blit(
+          source=self.big_font.render(
+            status_text[turn_step], 
+            True, 
+            'black'
+          ), 
+          dest=(20, 820)
+        )
+
+        for i in range(9):
+          pygame.draw.line(self.screen, 'black', (0, 100 * i), (800, 100 * i), 2)
+          pygame.draw.line(self.screen, 'black', (100 * i, 0), (100 * i, 800), 2)
+
+        self.screen.blit(self.medium_font.render('FORFEIT', True, 'black'), (810, 830))
+
   def create_white_pieces(self):
     for i in range(8):
       pawn = Pawn(
@@ -247,60 +303,3 @@ class Chess:
             run = False
       pygame.display.flip()
     pygame.quit()
-
-  def draw_board(self):
-    for i in range(32):
-      column = i % 4
-      row = i // 4
-      if row % 2 == 0:
-        pygame.draw.rect(
-          surface=self.screen, 
-          color='light gray', 
-          rect=[600 - (column * 200), row * 100, 100, 100]
-        )
-      else:
-        pygame.draw.rect(
-          surface=self.screen, 
-          color='light gray', 
-          rect=[700 - (column * 200), row * 100, 100, 100]
-        )
-
-      pygame.draw.rect(
-        surface=self.screen, 
-        color='gray', 
-        rect=[0, 800, WIDTH, 100]
-      )
-
-      pygame.draw.rect(
-        surface=self.screen, 
-        color='gold', 
-        rect=[0, 800, WIDTH, 100], 
-        width=5
-      )
-
-      pygame.draw.rect(
-        surface=self.screen, 
-        color='gold', 
-        rect=[800, 0, 200, HEIGHT], 
-        width=5
-      )
-
-      status_text = [
-        'White: Select a Piece to Move!', 'White: Select a Destination!',
-        'Black: Select a Piece to Move!', 'Black: Select a Destination!'
-      ]
-
-      self.screen.blit(
-        source=self.big_font.render(
-          status_text[turn_step], 
-          True, 
-          'black'
-        ), 
-        dest=(20, 820)
-      )
-
-      for i in range(9):
-        pygame.draw.line(self.screen, 'black', (0, 100 * i), (800, 100 * i), 2)
-        pygame.draw.line(self.screen, 'black', (100 * i, 0), (100 * i, 800), 2)
-
-      self.screen.blit(self.medium_font.render('FORFEIT', True, 'black'), (810, 830))
